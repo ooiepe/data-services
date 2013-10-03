1b5cbb77185c1079b82ca0513376b0997b7497d6
Sage Lichtenwalner <sage@marine.rutgers.edu>
2013-10-03 13:50:46 -0400

    Added csv export of data requests and Slim/http caching

--- data.php
+++ data.php
@@ -8,25 +8,6 @@ $app = new \Slim\Slim(array(
 ));
 
 /**
- * Test request
- * Path: /test
- */
-
-$app->get('/test', 'test1');
-function test1() {
-  $app = \Slim\Slim::getInstance();
-  $req = $app->request();
-
-  $valid = new validateParams();
-  $network = $valid->validateNetwork($req->get('network'));
-  print_r($network);
-  if (count($valid->exceptions) > 0) {
-    echo '{"error":'. json_encode($valid->exceptions) .'}';
-    exit;
-  }
-}
-
-/**
  * Data Request
  * Path: /
  */
@@ -36,10 +17,10 @@ function test1() {
 $app->get('/', 'getData');
 function getData() {
   global $normalizedParameters;
-  
+
   $app = \Slim\Slim::getInstance();
   $req = $app->request();
-  
+
   // Step 1 - Validate Parameters
   $valid = new validateParams();
 	$valid->validateNetworks($req->get('network'));
@@ -57,7 +38,11 @@ function getData() {
   $parameter_response = $normalizedParameters[$network][$parameter]['response'];  
   $dates['start_time'] = $valid->start_time;
   $dates['end_time'] = $valid->end_time;
-  
+
+  // HTTP Caching
+  $app->etag($network . $station . $parameter . $dates['start_time'] . $dates['end_time']);
+  $app->expires('+2 weeks');
+
   // Step 2 - If period > 30 days, break up requests
   $max_interval = 60*60*24*30;
   if ($dates['end_time']-$dates['start_time']>$max_interval) {
@@ -74,8 +59,8 @@ function getData() {
       $urls[$i] = 'http://opendap.co-ops.nos.noaa.gov/ioos-dif-sos/SOS?request=GetObservation&service=SOS&version=1.0.0&offering=urn:ioos:station:NOAA.NOS.CO-OPS:' . $station . '&observedproperty=' . $parameter_request . '&responseformat=text/csv&eventtime=' . gmdate("Y-m-d\TH:i:s\Z",$reqs[$i]) . '/' . gmdate("Y-m-d\TH:i:s\Z",$reqs[$i+1]);  
     }  
   }
-  echo "<pre>";
-  print_r($urls);
+  //echo "<pre>";
+  //print_r($urls);
 
   // Step 3 - Request data asynchronously and cat it together
   $data = request_data($urls);
@@ -103,16 +88,17 @@ function getData() {
   }
 
   // Step 5 - Parse data and extract only the desired variable column
+  $dataout[] = array('date',"$parameter ($uomOrig)");
   $datalines = explode("\n",$data);
   foreach ($datalines as $dataline) {
     $d = str_getcsv($dataline); 
     if (count($d)>1) {
-      $v = $d[$col2idx["$parameter_response ($uomOrig)"]];
-      $out[$d[$col2idx['date_time']]] = $v;  
+      $dataout[] = array($d[$col2idx['date_time']], $d[$col2idx["$parameter_response ($uomOrig)"]]);
     }
   }
-  //echo json_encode($out);
-  print_r($out);
+
+  // Write the CSV file
+  csv_output($dataout);
 
 }
 
@@ -120,15 +106,26 @@ function getData() {
 $app->run();
 
 
+/* csv_output
+ * Adapted from http://php.net/manual/en/function.fputcsv.php
+ */
+function csv_output($data) {
+  $outstream = fopen("php://output", 'w');
+  function __outputCSV(&$vals, $key, $filehandler) {
+    fputcsv($filehandler, $vals, ',', '"');
+  }
+  array_walk($data, '__outputCSV', $outstream);
+  fclose($outstream);
+}
 
-
-/* get_data
+/* request_data 
+ * Request data asynchronously from third-party web services using curl_multi
  * Adapted from http://www.sitepoint.com/using-curl-for-remote-requests/
  */
 function request_data($urls) {
   // initialize the multihandler
   $session = curl_multi_init();
- 
+
   $channels = array();
   foreach ($urls as $key => $url) {
     // initiate individual channel
@@ -144,15 +141,15 @@ function request_data($urls) {
     //echo curl_exec($channels[$key]);
     //curl_close($channels[$key]);
   }
- 
+
   // execute - if there is an active connection then keep looping
   $active = null;
   do {
     $status = curl_multi_exec($session, $active);
     curl_multi_select($session);
   } while ($active>0);
-  
   $response = '';
+
   // echo the content, remove the handlers, then close them
   foreach ($channels as $chan) {
     $response .= curl_multi_getcontent($chan);
@@ -160,10 +157,9 @@ function request_data($urls) {
     curl_multi_remove_handle($session, $chan);
     //curl_close($chan);
   }
- 
+
   // close the multihandler
   curl_multi_close($session);
-  
   return $response;
 }
 
